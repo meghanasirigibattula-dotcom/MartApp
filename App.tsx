708d109b20c34de5109302e3d3fa74c7f4ad0f7f
meghana <meghanasirigibattula@gmail.com>
2026-09-23 15:18:16 +0530

    app changes

--- App.tsx
+++ App.tsx
@@ -5,8 +5,7 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -28,10 +27,7 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text>Hello, React Native!</Text>
     </View>
   );
 }
