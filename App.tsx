18e80b3d35559f4ebfaf9489ec1d8d8f1a757820
meghana <meghanasirigibattula@gmail.com>
2026-09-23 22:06:40 +0530

    app ui

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@
  *
  * @format
  */
-
-import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text, Pressable } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -26,15 +25,26 @@ function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
   return (
-    <View style={styles.container}>
-      <Text>Hello, React Native!</Text>
-    </View>
+  <View style={styles.container}>
+ <Text style={styles.title}>MartApp</Text>
+  <Text>Old clothes exchange</Text>
+<Pressable onPress={() => console.log('Exchange clicked')}>
+  <Text>Click to exchange</Text>
+</Pressable>
+</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
 
